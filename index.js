55e8fb30ac8f504adb31ac5816a9113e83ab29ea
Devashish Kumar <dk3127197@gmail.com>
2025-05-10 17:05:16 +0530

    ;ocalhost frontend commented out

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "50mb" }));
 // Whitelist your frontend domain
 const allowedOrigins = [
   "https://school-management-fqyzvx3ed-devashishkr3s-projects.vercel.app",
-  "http://localhost:5173",
+  // "http://localhost:5173",
   "https://school-management-eta-one.vercel.app",
 ];
 // Add production domain when deploying
