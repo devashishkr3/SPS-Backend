4c8fcc812f6f81cb7f490e082860cb7a29fdd81e
Devashish Kumar <dk3127197@gmail.com>
2025-08-03 18:21:20 +0530

    updated

--- middlewares/admin/studentRegisterValidation.js
+++ middlewares/admin/studentRegisterValidation.js
@@ -6,10 +6,6 @@ const studentRegisterValidation = (req, res, next) => {
     name: Joi.string().required().messages({
       "string.empty": "Name is required.",
     }),
-    // password: Joi.string().min(6).required().messages({
-    //   "string.empty": "Password is required.",
-    //   "string.min": "Password must be at least 6 characters long.",
-    // }),
     DOB: Joi.date().required().messages({
       "date.base": "DOB must be a valid date.",
       "any.required": "DOB is required.",
